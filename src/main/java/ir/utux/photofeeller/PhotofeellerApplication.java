e82978babaf46fcfbf2b433aa0f83f72c462c953
malekpouri <m.malekpoor@gmail.com>
2021-11-28 08:40:24 +0330

    start new project

---
+++ src/main/java/ir/utux/photofeeller/PhotofeellerApplication.java
@@ -0,0 +1,13 @@
+package ir.utux.photofeeller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotofeellerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotofeellerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/utux/photofeeller/PhotofeellerApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.utux.photofeeller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotofeellerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
